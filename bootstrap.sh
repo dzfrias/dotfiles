944c7fa29bb529c3b260cb6eefe5403cf54a09af
Diego <96022404+Diego17230@users.noreply.github.com>
2022-06-03 21:30:02 -0700

    Removed plug.vim in favor of a remote install

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,6 +26,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Clone the non-core plugins
 source $HOME/dotfiles/.zsh_stuff/omzplugs.zsh
 
+curl -fLo ~/dotfiles/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
