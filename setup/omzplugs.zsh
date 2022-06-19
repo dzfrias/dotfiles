c018a24da21dcdc468ad937addf53a7aa2d26d2d
Diego <96022404+Diego17230@users.noreply.github.com>
2022-06-18 18:23:33 -0700

    Added omzplugs.zsh again

---
+++ setup/omzplugs.zsh
@@ -0,0 +1,14 @@
+# Installs oh-my-zsh plugins that are not already provided
+
+cd ${ZSH_CUSTOM1:-$ZSH/custom}/plugins
+
+# Gives a notification when an alias could've been used
+git clone https://github.com/djui/alias-tips.git
+# Autoupdates oh-my-zsh plugins
+git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
+# Gives suggestions for commands
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Autopairs
+git clone https://github.com/hlissner/zsh-autopair ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autopair
