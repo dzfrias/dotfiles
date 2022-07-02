1cff5ef5cb3c6b44401551ff6043f21e31d5f19d
Diego <96022404+dzfrias@users.noreply.github.com>
2022-07-01 20:59:21 -0700

    Add --decorate option to glog alias

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -9,7 +9,7 @@ alias gamm='git commit --amend'
 alias gtod='git rev-list --count --after=today00:00:00 --all'
 alias gc='git add --all && git commit --verbose'
 alias gprv='git push && repoview'
-alias glog="nvim +'Git log --oneline | execute \"normal! \<C-w>o\"'"
+alias glog="nvim +'Git log --oneline --decorate | execute \"normal! \<C-w>o\"'"
 
 # -GITHUB-
 alias ghl='ghlink'
