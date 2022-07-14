1db39e97d6602bdabffa145e4ef24ec3c96685f2
Diego <96022404+dzfrias@users.noreply.github.com>
2022-07-14 11:05:01 -0700

    macos: Add macos default to enable quitting finder

--- misc/macos.zsh
+++ misc/macos.zsh
@@ -9,3 +9,6 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Disable press and hold
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Make finder quit with CMD-Q
+defaults write com.apple.finder QuitMenuItem -bool true
