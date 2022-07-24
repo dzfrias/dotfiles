f0db6beaa6a1a9a5d25d3c9620fbcb93e7631fc7
Diego <96022404+dzfrias@users.noreply.github.com>
2022-07-23 21:12:55 -1000

    zsh(aliases): Clean up nvim
    Remove extraneous comments and rename `plugged` to `plug`

--- zsh/aliases/nvim.zsh
+++ zsh/aliases/nvim.zsh
@@ -1,17 +1,10 @@
 alias n='nvim'
 alias nm='nvimm'
-# Edits zshrc
 alias zrc="nvim $HOME/.zshrc"
-# Edits extra zsh config file
 alias ezrc="nvim $HOME/.dotfiles/zsh/extra.zsh"
-# Edits init.vim
 alias vrc="nvim $HOME/.dotfiles/vim/nvim/init.vim"
-# Edits vimrc
 alias evrc="nvim $HOME/.dotfiles/vim/vimrc"
-# Views the file with a Markdown renderer
-alias mrkd="command nvim $1 -c 'MarkdownPreview'"
-alias plugc='nvim +PlugClean +qall'
-alias plugi='nvim +PlugInstall +qall'
-alias plugu='nvim +PlugUpdate +qall'
-# Goes to directory with plugins
-alias plugged='cd ~/.local/share/nvim/plugged'
+alias plug='cd ~/.local/share/nvim/plugged'
+alias plugc='nvim +PlugClean'
+alias plugi='nvim +PlugInstall'
+alias plugu='nvim +PlugUpdate'
