e5c6b354bbafba9f6a3832ce006c5f5765cdeea9
Diego <96022404+dzfrias@users.noreply.github.com>
2022-07-28 21:04:23 -0700

    zsh(aliases): Add `no` alias

--- zsh/aliases/nvim.zsh
+++ zsh/aliases/nvim.zsh
@@ -1,9 +1,14 @@
 alias n='nvim'
 alias nm='nvimm'
+alias no="nvim -c 'execute \"edit\" . MruGetFiles()[0]'"
+
+# quick access
 alias zrc="nvim $HOME/.zshrc"
 alias ezrc="nvim $HOME/.dotfiles/zsh/extra.zsh"
 alias vrc="nvim $HOME/.dotfiles/vim/nvim/init.vim"
 alias evrc="nvim $HOME/.dotfiles/vim/vimrc"
+
+# vim-plug
 alias plug='cd ~/.local/share/nvim/plugged'
 alias plugc='nvim +PlugClean'
 alias plugi='nvim +PlugInstall'
