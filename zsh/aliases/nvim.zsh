3da79665bbbc5e1f22f162f20600e2d951114bda
Diego <96022404+dzfrias@users.noreply.github.com>
2022-07-31 13:04:51 -0700

    zsh: Change vrc alias
    Make it go to the nvim/lua directory

--- zsh/aliases/nvim.zsh
+++ zsh/aliases/nvim.zsh
@@ -2,10 +2,10 @@ alias n='nvim'
 alias nm='nvimm'
 alias no="nvim -c 'execute \"edit\" . MruGetFiles()[0]'"
 
-# quick access
+# Quick access
 alias zrc="nvim $HOME/.zshrc"
 alias ezrc="nvim $HOME/.dotfiles/zsh/extra.zsh"
-alias vrc="nvim $HOME/.dotfiles/vim/nvim/init.vim"
+alias vrc="j ~/.dotfiles/vim/nvim/lua"
 alias evrc="nvim $HOME/.dotfiles/vim/vimrc"
 
 # vim-plug
