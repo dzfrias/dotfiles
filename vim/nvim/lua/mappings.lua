f55790262cc12767842059acdb01e6aa79450a97
Diego <96022404+dzfrias@users.noreply.github.com>
2022-07-31 12:54:35 -0700

    vim: Add comments to settings and mappings

--- vim/nvim/lua/mappings.lua
+++ vim/nvim/lua/mappings.lua
@@ -10,48 +10,66 @@ local function map(shortcut, command)
 end
 
 
+-- Go to top of file and first character
 map('gg', 'gg0')
 
+-- Fold movement down
 map('<s-j>', 'zj')
+-- Open fold
 map('<space>', 'za')
 
+-- Go to last place in insert mode
 map('gi', 'gi<Esc>')
 
+-- Tab movement
 map('<C-l>', '<Cmd>tabn<CR>')
 map('<C-h>', '<Cmd>tabp<CR>')
 
+-- Better wrapped line movement
 map('k', 'gk')
 map('j', 'gj')
 map('0', 'g0')
 map('$', 'g$')
 
+-- Make a newline
 nmap('<CR>', 'o<Esc>')
+
+-- Basic command mappings
 nmap('<leader>w', '<Cmd>write<CR>')
 nmap('<leader>s', '<Cmd>source %<CR>')
 nmap('<leader>q', '<Cmd>quit!<CR>')
 nmap('<leader>e', '<Cmd>write<CR><Cmd>source $MYVIMRC<CR>')
 
+-- Re-edit file, also loading the entire runtime
 nmap('<leader>v', '<Cmd>Runtime<CR><Cmd>edit<CR>')
 
+-- Fzf files at the project root, see ProjFiles in commands.lua
 nmap('<leader>f', '<Cmd>ProjFiles<CR>')
 
+-- Open NERDTree
 nmap('<C-n>', '<Cmd>NERDTreeToggle<CR>')
 
+-- Git mappings
 nmap('gq', '<Cmd>Git<CR>')
 nmap('gcm', '<Cmd>Git add --all <bar> Git commit --verbose<CR>')
 nmap('gp', '<Cmd>Git push<CR>')
 
-imap('jk', '<Esc>')
-
+-- Vimspector launch settings, see all in plugins/vimspector.lua
 nmap('<leader>du', '<Cmd>call vimspector#LaunchWithSettings(#{configuration: "test"})<CR>')
 nmap('<leader>dd', '<Cmd>call vimspector#LaunchWithSettings(#{configuration: "run"})<CR>')
 
+-- Window movement
 nmap('<leader>k', '<C-w>k')
 nmap('<leader>l', '<C-w>l')
 nmap('<leader>h', '<C-w>h')
 nmap('<leader>j', '<C-w>j')
 
+-- Open floating terminal and run call callcmd on file
 nmap('<leader>r', '<Cmd>w<CR><Cmd>lua require("FTerm").run({vim.b.callcmd, "&&", "read -n 1 && exit"})<CR>')
 
+-- Escape
+imap('jk', '<Esc>')
+
+-- Tab movement
 imap('<C-l>', '<Esc><Cmd>tabn<CR>')
 imap('<C-h>', '<Esc><Cmd>tabp<CR>')
--- vim/nvim/lua/settings.lua
+++ vim/nvim/lua/settings.lua
@@ -1,43 +1,59 @@
 local set = vim.o
 
-set.hidden = true
 
+-- No backups
 set.writebackup = false
 set.backup = false
 
+-- Number settings
 set.relativenumber = true
 
+-- Merge signcolumn with the number column
 set.signcolumn = 'number'
 
+-- Highlight search matches
 set.hlsearch = true
 
+-- Color column
 set.colorcolumn = '80'
 vim.cmd[[highlight ColorColumn ctermbg=0 guibg=lightgrey]]
 
+--  Lines to use for the command line
 set.cmdheight = 2
 
+-- Don't wrap lines
 set.wrap = false
 
-set.shortmess = set.shortmess .. 'c'
+vim.opt.shortmess:append({ c = true })
 
+-- For colors
 set.background = 'dark'
 
+-- Line cursor in insert and command mode
 set.guicursor = 'c:ver20,i:ver20,ci:ver20,cr:ver20'
 
+-- Updating time, useful for plugins
 set.updatetime = 100
 
+-- Use system clipboard
 set.clipboard = 'unnamed'
 
+-- Indent based on folds
 set.foldmethod = 'indent'
 set.foldlevelstart = 2
+-- Max 1 fold level
 set.foldnestmax = 1
+-- Don't open folds with block movement ('{' and '}')
 set.foldopen = set.foldopen:gsub('block,', '')
 
+-- Indentation
 set.autoindent = true
 set.smartindent = true
 set.expandtab = true
 
 set.modeline = true
+-- Only one modeline allowed
 set.modelines = 1
 
+-- Completion window max items
 set.pumheight = 5
