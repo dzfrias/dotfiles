18250c6c50b2bbe2fedcaa1d0000958eb7db013e
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-03 09:18:58 -0700

    vim: Update emmet binding in css.lua

--- vim/nvim/ftplugin/css.lua
+++ vim/nvim/ftplugin/css.lua
@@ -2,5 +2,5 @@ local function bufmap(mode, key, cmd)
   vim.api.nvim_buf_set_keymap(0, mode, key, cmd, { noremap = true })
 end
 
-bufmap('i', '<C-v>', '<Plug>(emmet-expand-abbr)')
+bufmap('i', 'ee', '<Plug>(emmet-expand-abbr)')
 bufmap('n', '<C-v>', '<Plug>(emmet-expand-abbr)')
