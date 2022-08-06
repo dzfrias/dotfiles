458895b4bd435dd3e962fc42afddcb81aaa07e12
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-06 10:28:57 -0700

    vim: Change telescope config

--- vim/nvim/lua/plugins/telescope.lua
+++ vim/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,18 @@
-require('telescope').setup{
+actions = require('telescope.actions')
+
+require('telescope').setup {
   defaults = {
+    layout_strategy = 'vertical',
+    layout_config = {
+      vertical = { width = 0.7 }
+    },
     mappings = {
       i = {
         ['<C-u>'] = false,
-      },
-    },
+        ['<C-d>'] = false,
+        ['<C-f>'] = actions.preview_scrolling_down,
+        ['<C-b>'] = actions.preview_scrolling_up
+      }
+    }
   }
 }
