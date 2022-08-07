2b59182fbe8c0fc2af26d2b15709666b32df19f0
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-06 17:12:44 -0700

    vim(util): Change implementation of get_proj_root
    Much cleaner, uses vim builtins instead.

--- vim/nvim/lua/util.lua
+++ vim/nvim/lua/util.lua
@@ -1,22 +1,7 @@
 local M = {}
 
-local function get_project_root(dir)
-  dir = vim.fn.fnamemodify(dir, ':p'):sub(1, -2)
-  home = os.getenv 'HOME'
-
-  if dir == home or dir == '' then
-    return '.'
-  end
-  if vim.fn.isdirectory(dir .. '/.git') == 1 then
-    return dir
-  end
-
-  pdir = vim.fn.fnamemodify(dir, ':h')
-  return get_project_root(pdir)
-end
-
 function M.get_project_root()
-  return get_project_root '.'
+  return vim.fn.fnamemodify(vim.fn.finddir('.git', '.;'), ':h:p')
 end
 
 function M.nnoremap(shortcut, command)
