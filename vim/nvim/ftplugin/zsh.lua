b31a7fb75be11a0c0f65c6487f281c8708b880c2
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-08 12:56:13 -0700

    vim: Remove zshrc filetype

--- vim/nvim/ftplugin/zsh.lua
+++ vim/nvim/ftplugin/zsh.lua
@@ -4,3 +4,7 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 
 vim.b.callcmd = 'zsh ' .. vim.fn.expand '%'
+
+if vim.fn.expand('%'):find 'zshrc' then
+  vim.cmd 'normal! zM'
+end
--- vim/nvim/ftplugin/zshrc.lua
+++
@@ -1,1 +0,0 @@
-vim.api.nvim_command 'normal! zM'
