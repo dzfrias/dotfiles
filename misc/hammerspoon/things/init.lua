6193e9c461c1bd46ba99bb90092fc7079af3670b
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-09 09:12:07 -0700

    hs(things): Add comments to keybinds

--- misc/hammerspoon/things/init.lua
+++ misc/hammerspoon/things/init.lua
@@ -1,35 +1,43 @@
 local u = require 'util'
 
+-- Go down
 local down = hs.hotkey.new({ 'ctrl' }, 'J', function()
   u.key_stroke('Down')
 end)
 
+-- Go up
 local up = hs.hotkey.new({ 'ctrl' }, 'K', function()
   u.key_stroke('Up')
 end)
 
+-- Go out of current scope
 local out_of = hs.hotkey.new({ 'ctrl' }, 'H', function()
   u.key_stroke({ 'cmd' }, 'Left')
 end)
 
+-- Go into a nested scope
 local into = hs.hotkey.new({ 'ctrl' }, 'L', function()
   u.key_stroke('Return')
 end)
 
+-- Reorder item up
 local reorder_up = hs.hotkey.new({ 'ctrl', 'shift' }, 'K', function()
   u.key_stroke({ 'cmd' }, 'Up')
 end)
 
+-- Reorder item down
 local reorder_down = hs.hotkey.new({ 'ctrl', 'shift' }, 'J', function()
   u.key_stroke({ 'cmd' }, 'Down')
 end)
 
+-- Go to the `School` area
 local school = hs.hotkey.new({ 'cmd' }, '8', function()
   hs.eventtap.keyStroke({ 'cmd' }, 'F')
   hs.eventtap.keyStrokes('school')
   hs.eventtap.keyStroke({}, 'Return')
 end)
 
+-- Go to the `Coding` area
 local coding = hs.hotkey.new({ 'cmd' }, '9', function()
   hs.eventtap.keyStroke({ 'cmd' }, 'F')
   hs.eventtap.keyStrokes('coding')
