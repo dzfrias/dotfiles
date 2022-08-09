14657241d8795535e856ee5fbe6e1350a63e79c0
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-09 08:20:28 -0700

    hammerspoon(chrome): Add bookmarks

--- misc/hammerspoon/chrome/init.lua
+++ misc/hammerspoon/chrome/init.lua
@@ -1,27 +1,56 @@
 local u = require('util')
 
+-- Previous tab
 local tabp = hs.hotkey.new({ 'ctrl' }, 'J', function()
   u.keyStroke({ 'cmd', 'alt' }, 'Left')
 end)
 
+-- Next tab
 local tabn = hs.hotkey.new({ 'ctrl' }, 'K', function()
   u.keyStroke({ 'cmd', 'alt' }, 'Right')
 end)
 
+-- Fuzzy find bookmarks
 local bookmarks = hs.hotkey.new({ 'ctrl' }, 'B', function()
-  local old_pos = hs.mouse.absolutePosition()
-  hs.eventtap.leftClick({ x = 141, y = 95 }, 0)
-  hs.mouse.absolutePosition(old_pos)
+  local chrome = hs.application.get('Google Chrome')
+  local items = chrome:getMenuItems()
+  local bookmark_menu = items[6].AXChildren[1]
+
+  local bookmarks = {}
+  for i, item in ipairs(bookmark_menu) do
+    if i > 4 then
+      if item.AXChildren then
+        for _, child in ipairs(item.AXChildren[1]) do
+          local choice = { text = child.AXTitle }
+          table.insert(bookmarks, choice)
+        end
+      else
+        local choice = { text = item.AXTitle }
+        table.insert(bookmarks, choice)
+      end
+    end
+  end
+
+  local chooser = hs.chooser.new(function(choice)
+    chrome:activate()
+    chrome:selectMenuItem('New Tab')
+    chrome:selectMenuItem(choice.text)
+  end)
+  chooser:choices(bookmarks)
+  chooser:show()
 end)
 
+-- Fuzzy find tabs
 local tabs = hs.hotkey.new({ 'ctrl' }, 'T', function()
   u.keyStroke({ 'cmd', 'shift' }, 'A')
 end)
 
+-- Go up
 local up = hs.hotkey.new({ 'ctrl', 'shift' }, 'K', function()
   u.keyStroke('Up')
 end)
 
+-- Go down
 local down = hs.hotkey.new({ 'ctrl', 'shift' }, 'J', function()
   u.keyStroke('Down')
 end)
