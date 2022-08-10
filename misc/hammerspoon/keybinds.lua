8313c4f204c2b3c71d61d1aeed443c86910d109c
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-09 21:06:33 -0700

    hs: Add keybinding for clearing notifications
    Credits to:
    https://apple.stackexchange.com/questions/408019/dismiss-macos-big-sur-notifications-with-keyboard

--- misc/hammerspoon/keybinds.lua
+++ misc/hammerspoon/keybinds.lua
@@ -58,6 +58,51 @@ hs.hotkey.bind({ 'alt', 'ctrl' }, 'L', function()
   hs.caffeinate.lockScreen()
 end)
 
+-- Clear all notifications
+hs.hotkey.bind({ 'alt', 'ctrl' }, 'N', function()
+  hs.applescript [[
+  activate application "NotificationCenter"
+  tell application "System Events"
+      tell process "Notification Center"
+          repeat
+              
+              try
+                  set theWindow to group 1 of UI element 1 of scroll area 1 of window "Notification Center"
+              on error
+                  exit repeat
+              end try
+              
+              try
+                  set theActions to actions of theWindow
+                  
+                  # Try to close the whole group first. If that fails, close individual windows.
+                  repeat with theAction in theActions
+                      if description of theAction is "Clear All" then
+                          set closed to true
+                          tell theWindow
+                              perform theAction
+                          end tell
+                          exit repeat
+                      end if
+                  end repeat
+                  
+                  repeat with theAction in theActions
+                      if description of theAction is "Close" then
+                          set closed to true
+                          tell theWindow
+                              perform theAction
+                          end tell
+                          exit repeat
+                      end if
+                  end repeat
+                  
+              end try
+          end repeat
+      end tell
+  end tell
+  ]]
+end)
+
 -- Toggle iTerm and Chrome vertical split
 hs.hotkey.bind({ 'alt', 'ctrl' }, 'D', function()
   local chrome = hs.appfinder.appFromName('Google Chrome'):mainWindow()
