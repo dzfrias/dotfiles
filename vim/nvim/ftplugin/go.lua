cf98ed6977a12e6702ba43603ed646bea3e1f315
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-10 18:03:59 -0700

    vim: Update go keymaps
    Remove <C-p> for godoc lookup and change GoIfErr bindings.

--- vim/nvim/ftplugin/go.lua
+++ vim/nvim/ftplugin/go.lua
@@ -6,10 +6,9 @@ vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
 -- vim-go
 bufnoremap('n', '<leader>r', '<Cmd>w<CR><Cmd>GoRun<CR>')
 bufnoremap('n', '<leader>T', '<Cmd>w<CR><Cmd>GoTest<CR>')
-bufnoremap('n', '<C-p>', '<Plug>(go-doc)')
 bufnoremap('n', '<leader>c', '<Plug>(go-coverage-toggle)')
-bufnoremap('n', '<C-i>', '<Cmd>GoIfErr<CR>k')
-bufnoremap('i', '<C-i>', '<Esc><Cmd>GoIfErr<CR>k')
+bufnoremap('n', '<C-e>', '<Cmd>GoIfErr<CR>2kA ')
+bufnoremap('i', '<C-e>', '<Esc><Cmd>GoIfErr<CR>2kA ')
 
 -- Gojson
 bufnoremap('n', '<leader>g', '<Plug>Gojson')
