0bb65685047260ef7d9e74b308564e974a05cf4f
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-12 11:44:38 -0700

    vim(overseer): Add some mappings for the task list

--- vim/nvim/lua/dzfrias/plugins/overseer.lua
+++ vim/nvim/lua/dzfrias/plugins/overseer.lua
@@ -1,8 +1,13 @@
 require('overseer').setup {
   task_list = {
     bindings = {
-      ['<M-l>'] = 'IncreaseDetail',
-      ['<M-h>'] = 'DecreaseDetail',
+      ['<M-l>'] = 'IncreaseDetailAll',
+      ['<M-h>'] = 'DecreaseDetailAll',
+      ['L'] = 'IncreaseDetail',
+      ['H'] = 'DecreaseDetail',
+      ['q'] = '<Cmd>quit<CR>',
+      ['K'] = 'PrevTask',
+      ['J'] = 'NextTask',
     },
   },
   templates = { 'builtin', 'run' },
