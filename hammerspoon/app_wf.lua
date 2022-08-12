0839d99e275379dc206ab816ee0691084d47e975
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-12 09:26:09 -0700

    hs(app_wf): Reimplement the `start` function
    Do a lookup instead of a loop.

--- hammerspoon/app_wf.lua
+++ hammerspoon/app_wf.lua
@@ -14,27 +14,21 @@ function M.start()
   M.global_wf
     :subscribe(
       { hs.window.filter.windowFocused, hs.window.filter.windowVisible },
-      function(_, app)
-        for app_name, keybinds in pairs(M.app_wfs) do
-          if app ~= app_name then
-            goto continue
-          end
-          for _, keybind in ipairs(keybinds) do
-            keybind:enable()
-          end
-          ::continue::
+      function(_, app_name)
+        if not M.app_wfs[app_name] then
+          return
+        end
+        for _, keybind in ipairs(M.app_wfs[app_name]) do
+          keybind:enable()
         end
       end
     )
-    :subscribe({ hs.window.filter.windowUnfocused }, function(_, app)
-      for app_name, keybinds in pairs(M.app_wfs) do
-        if app ~= app_name then
-          goto continue
-        end
-        for _, keybind in ipairs(keybinds) do
-          keybind:disable()
-        end
-        ::continue::
+    :subscribe({ hs.window.filter.windowUnfocused }, function(_, app_name)
+      if not M.app_wfs[app_name] then
+        return
+      end
+      for _, keybind in ipairs(M.app_wfs[app_name]) do
+        keybind:disable()
       end
     end)
 end
