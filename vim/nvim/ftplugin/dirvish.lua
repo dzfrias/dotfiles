1668044540f9816773d8ee262c82f2b0741e8711
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-12 11:38:54 -0700

    vim(dirvish): Change some mappings

--- vim/nvim/ftplugin/dirvish.lua
+++ vim/nvim/ftplugin/dirvish.lua
@@ -1,6 +1,7 @@
 local bufnoremap = require('dzfrias/util').bufnoremap
 
 bufnoremap('n', '<leader>n', ':!touch %')
-bufnoremap('n', '<leader>r', ':Shdo mv {}')
-bufnoremap('n', '<leader>d', '<Cmd>Shdo rm {}<CR>', { nowait = true })
+bufnoremap('n', '<leader>d', ':!mkdir %')
+bufnoremap('n', '<leader>m', ':Shdo mv {}')
+bufnoremap('n', '<leader>r', '<Cmd>Shdo rm {}<CR>', { nowait = true })
 bufnoremap('n', '<leader>s', ':Shdo ')
