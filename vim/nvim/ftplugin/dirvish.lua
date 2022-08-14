43732cd6082cf67a586ae6209de55c3e0fc30ded
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-14 12:03:16 -0700

    vim(dirvish): Add <leader>e keymap

--- vim/nvim/ftplugin/dirvish.lua
+++ vim/nvim/ftplugin/dirvish.lua
@@ -2,6 +2,7 @@ local bufnoremap = require('dzfrias/util').bufnoremap
 
 bufnoremap('n', '<leader>n', ':!touch %')
 bufnoremap('n', '<leader>d', ':!mkdir %')
+bufnoremap('n', '<leader>e', ':edit %')
 bufnoremap('n', '<leader>m', ':Shdo mv {}')
 bufnoremap('n', '<leader>r', '<Cmd>Shdo rm {}<CR>', { nowait = true })
 bufnoremap('n', '<leader>s', ':Shdo ')
