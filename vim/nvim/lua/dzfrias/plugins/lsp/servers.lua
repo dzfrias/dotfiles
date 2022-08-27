277014c580aa8bbd434d5dee89cc2b2a6a76693e
Diego <96022404+dzfrias@users.noreply.github.com>
2022-08-27 14:51:55 -0700

    vim(lsp): Add rust

--- vim/nvim/lua/dzfrias/plugins/lsp/servers.lua
+++ vim/nvim/lua/dzfrias/plugins/lsp/servers.lua
@@ -1,5 +1,5 @@
 -- Servers to install/setup
 return {
-  default = { 'pyright', 'gopls', 'vimls', 'jsonls' },
+  default = { 'pyright', 'gopls', 'vimls', 'jsonls', 'rust_analyzer' },
   special = { 'ltex', 'sumneko_lua' },
 }
--- vim/nvim/lua/dzfrias/plugins/treesitter.lua
+++ vim/nvim/lua/dzfrias/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'python', 'vim', 'go', 'lua', 'json', 'regex' },
+  ensure_installed = { 'python', 'vim', 'go', 'lua', 'json', 'regex', 'rust' },
 
   highlight = {
     enable = true,
