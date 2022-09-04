5ebb683ba6065c1f046a77e4ab90cc1b33c2e36f
Diego <96022404+dzfrias@users.noreply.github.com>
2022-09-04 16:43:40 -0700

    vim(rust): Set up debugger

--- vim/nvim/ftplugin/rust.lua
+++ vim/nvim/ftplugin/rust.lua
@@ -1,6 +1,10 @@
+local bufnoremap = require('dzfrias/util').bufnoremap
+
 vim.opt_local.colorcolumn = '80'
 vim.opt_local.foldmethod = 'expr'
 vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
+
+bufnoremap('n', '<leader>dd', '<Cmd>RustDebuggables<CR>')
 
 local autopairs = {}
 autopairs['('] = ')'
--- vim/nvim/lua/dzfrias/plugins/rust-tools.lua
+++ vim/nvim/lua/dzfrias/plugins/rust-tools.lua
@@ -1,4 +1,10 @@
 local rt = require 'rust-tools'
+local data = vim.fn.stdpath 'data'
+-- Path to liblldb, installed with Mason
+local liblldb = data
+  .. '/mason/packages/codelldb/extension/lldb/lib/liblldb.dylib'
+-- Path to codelldb, installed with Mason
+local codelldb = data .. '/mason/packages/codelldb/extension/adapter/codelldb'
 
 rt.setup {
   server = {
@@ -10,9 +16,7 @@ rt.setup {
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
       vim.keymap.set('n', '<leader>n', vim.lsp.buf.rename, bufopts)
 
-      -- Hover actions
       vim.keymap.set('n', '<s-k>', rt.hover_actions.hover_actions, bufopts)
-      -- Code action groups
       vim.keymap.set(
         'n',
         '<leader>a',
@@ -20,5 +24,10 @@ rt.setup {
         bufopts
       )
     end,
+  },
+
+  dap = {
+    -- Setup for codelldb
+    adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb, liblldb),
   },
 }
