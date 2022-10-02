0f4a4ae3bbe4971aae354654a31e15e99a687409
Diego <96022404+dzfrias@users.noreply.github.com>
2022-10-02 15:01:45 -0700

    misc: remove things_homework

--- misc/things_homework.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-days=(Monday Tuesday Wednesday Thursday Friday)
-for i in {0..4}; do
-  when="in%20$i%20days"
-  open "things:///add?title=${days[i]}%20homework&checklist-items=%20&when=$when&list=School"
-done
-open "things:///add?title=Weekend%20homework&checklist-items=%20&when=Saturday&list=School"
