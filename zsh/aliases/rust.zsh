f784850885c69968759ab1f9ed10a222e913c74f
Diego <96022404+dzfrias@users.noreply.github.com>
2022-11-28 15:02:47 -0800

    zsh(rust): add trunk serve alias

--- zsh/aliases/rust.zsh
+++ zsh/aliases/rust.zsh
@@ -1,3 +1,4 @@
+# Cargo
 alias temprs='cargo init --name temprs'
 alias c='cargo'
 alias cr='cargo run'
@@ -5,3 +6,6 @@ alias cra='cargo run --'
 alias ct='cargo nextest run'
 alias cdoc='cargo doc --open'
 alias rustc='~/.cargo/bin/rustc'
+
+# Trunk
+alias ts='trunk serve --open'
