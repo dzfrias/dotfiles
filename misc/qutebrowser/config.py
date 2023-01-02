2b14dc376a88379f99e46f7b7485cba527677b73
Diego <96022404+dzfrias@users.noreply.github.com>
2023-01-02 11:21:56 -0800

    qute: make color scheme better

--- misc/qutebrowser/config.py
+++ misc/qutebrowser/config.py
@@ -53,6 +53,11 @@ c.statusbar.widgets = [
     "progress",
 ]
 
+# Fonts
+c.fonts.messages.error = "bold default_size default_family"
+c.fonts.prompts = "default_size default_family"
+c.fonts.tabs.selected = "italic default_size default_family"
+
 # Tabs
 c.tabs.indicator.width = 8
 c.tabs.last_close = "startpage"
@@ -64,10 +69,14 @@ COLORS = {
     "white": "rgb(169, 177, 214)",
     "red": "rgb(247, 118, 142)",
     "purple": "rgb(187, 154, 247)",
+    "purple2": "#4d3872",
+    "magenta": "#bb9af7",
+    "orange": "#ff9e64",
     "black": "#16161e",
     "green": "#9ece6a",
     "blue": "#3d59a1",
     "dark blue": "#13265c",
+    "sky blue": "#b4f9f8",
     "light blue": "#b4f9f8",
 }
 
@@ -77,12 +86,24 @@ c.colors.hints.bg = COLORS["light blue"]
 c.colors.hints.match.fg = COLORS["light blue"]
 
 c.colors.messages.error.bg = COLORS["red"]
+c.colors.messages.warning.bg = COLORS["orange"]
+c.colors.messages.info.bg = COLORS["black"]
+c.colors.messages.info.fg = "white"
+c.colors.messages.warning.bg = COLORS["black"]
+c.colors.messages.warning.fg = COLORS["orange"]
+c.colors.messages.error.bg = COLORS["black"]
+c.colors.messages.error.fg = COLORS["red"]
 
-c.colors.statusbar.caret.bg = COLORS["purple"]
-c.colors.statusbar.command.bg = COLORS["black"]
+c.colors.statusbar.caret.bg = "#ff9e64"
+c.colors.statusbar.command.bg = "#071a4f"
+c.colors.statusbar.command.fg = "white"
 c.colors.statusbar.normal.bg = COLORS["black"]
 c.colors.statusbar.insert.bg = COLORS["green"]
 c.colors.statusbar.insert.fg = "black"
+c.colors.statusbar.url.hover.fg = COLORS["orange"]
+c.colors.statusbar.url.fg = COLORS["sky blue"]
+c.colors.statusbar.url.success.http.fg = COLORS["sky blue"]
+c.colors.statusbar.url.success.https.fg = COLORS["sky blue"]
 
 c.hints.border = "1px solid black"
 
@@ -90,18 +111,22 @@ c.colors.tabs.selected.even.bg = COLORS["blue"]
 c.colors.tabs.selected.odd.bg = COLORS["blue"]
 c.colors.tabs.odd.bg = COLORS["black"]
 c.colors.tabs.even.bg = COLORS["black"]
+c.colors.tabs.indicator.stop = COLORS["purple"]
+c.colors.tabs.indicator.error = COLORS["red"]
 
 c.colors.keyhint.suffix.fg = "aqua"
 
+c.colors.completion.fg = [COLORS["magenta"], "white", "white"]
 c.colors.completion.even.bg = "#24283b"
 c.colors.completion.odd.bg = "#24283b"
 c.colors.completion.match.fg = COLORS["green"]
-c.colors.completion.item.selected.match.fg = COLORS["red"]
-c.colors.completion.item.selected.fg = "white"
-c.colors.completion.category.bg = COLORS["dark blue"]
-c.colors.completion.category.border.bottom = COLORS["white"]
-
-c.colors.statusbar.url.success.https.fg = "white"
+c.colors.completion.item.selected.match.fg = COLORS["orange"]
+c.colors.completion.item.selected.fg = "#d3d3d3"
+c.colors.completion.item.selected.border.bottom = "aqua"
+c.colors.completion.item.selected.border.top = "aqua"
+c.colors.completion.category.bg = COLORS["black"]
+c.colors.completion.category.fg = COLORS["sky blue"]
+c.colors.completion.category.border.bottom = "cyan"
 
 # Other options
 c.editor.command = [
