1c5003c70665348b99425b4336005cfdce80e221
Diego <96022404+dzfrias@users.noreply.github.com>
2023-01-29 09:01:48 -0800

    zsh: add `rec` alias

--- zsh/aliases/zsh.zsh
+++ zsh/aliases/zsh.zsh
@@ -18,6 +18,7 @@ alias license='cp ~/.dotfiles/LICENSE .'
 alias cl='j && clear'
 # Clear mbox file
 alias clmb='echo "" > ~/.mbox'
+alias rec='mv "$HOME/Downloads/"$(cd ~/Downloads; ls -Art | tail -n 1) .'
 
 # -CLI-OPTIONS-
 # Doesn't open pager for bat (default is 'always' in misc/config/bat/config)
