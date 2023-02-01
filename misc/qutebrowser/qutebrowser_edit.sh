7ab9c2c4b4ed5ae8f2015b20d3743594e38a32ed
Diego <96022404+dzfrias@users.noreply.github.com>
2023-01-31 18:04:49 -0800

    qutebrowser: remove config

--- misc/qutebrowser/qutebrowser_edit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/opt/homebrew/bin/alacritty -e /opt/homebrew/bin/nvim -f "$1"
--- misc/qutebrowser/userscripts/open_in_safari.py
+++
@@ -1,6 +0,0 @@
-#!/opt/homebrew/bin/python3
-
-import os
-
-url = os.environ["QUTE_URL"]
-os.system(f"open -a Safari {url}")
