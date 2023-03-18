797666fef1cd3ab995266be8f1f4dd6db16e8fd1
Diego <96022404+dzfrias@users.noreply.github.com>
2023-03-18 09:13:42 -0700

    zsh(rust): add clippy alias

--- zsh/aliases/rust.zsh
+++ zsh/aliases/rust.zsh
@@ -6,6 +6,7 @@ alias cra='cargo run --'
 alias ct='cargo nextest run'
 alias cdoc='cargo doc --open'
 alias rustc='~/.cargo/bin/rustc'
+alias ccl='cargo clippy --all'
 
 # Trunk
 alias ts='trunk serve --open'
