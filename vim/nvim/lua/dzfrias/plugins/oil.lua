8a76132b307db757c1bc2dce34d1053b21fe6f70
Diego <96022404+dzfrias@users.noreply.github.com>
2023-04-04 09:40:04 -0700

    vim(oil): change some key bindings

--- vim/nvim/lua/dzfrias/plugins/oil.lua
+++ vim/nvim/lua/dzfrias/plugins/oil.lua
@@ -2,5 +2,8 @@ return {
   keymaps = {
     ['<C-v>'] = 'actions.select_vsplit',
     ['<C-x>'] = 'actions.select_split',
+    ['<C-h>'] = '<Cmd>wincmd h<CR>',
+    ['<C-l>'] = '<Cmd>wincmd l<CR>',
+    ['q'] = 'actions.close',
   },
 }
