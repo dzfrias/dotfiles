0b353e25ab833492f43eec882ee460bfd3501dab
Diego <96022404+dzfrias@users.noreply.github.com>
2023-05-26 16:20:34 -0700

    zsh: add sgmv

--- zsh/autoload.zsh
+++
@@ -1,7 +0,0 @@
-# Autoloads every file in the autoload directory
-
-# Adds directory to fpath
-fpath=("$HOME/.dotfiles/zsh/autoload" $fpath)
-
-# Autloads all regular files
-autoload ~/.dotfiles/zsh/autoload/*(.)
