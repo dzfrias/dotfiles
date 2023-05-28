315a40d7f5278aa6cceec71a7fe152de808ca32c
Diego <96022404+dzfrias@users.noreply.github.com>
2023-05-28 16:05:41 -0700

    vim: use telescope for `fn`

--- zsh/aliases/nvim.zsh
+++ zsh/aliases/nvim.zsh
@@ -11,3 +11,4 @@ alias trc='nvim ~/.dotfiles/misc/tmux.conf'
 alias evrc="nvim $HOME/.dotfiles/vim/vimrc"
 alias nvdir='j ~/.local/share/nvim'
 alias hs='j ~/.dotfiles/hammerspoon'
+alias fn='nvim "+lua require(\"telescope.builtin\").find_files { cwd = require(\"dzfrias.util\").get_project_root() }"'
