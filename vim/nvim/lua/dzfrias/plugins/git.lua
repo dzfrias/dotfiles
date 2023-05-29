58d0ef7450665a3418af175e8860b94f1fbf1bcd
Diego <96022404+dzfrias@users.noreply.github.com>
2023-05-29 07:47:32 -0700

    vim: add lazygit back

--- vim/nvim/lua/dzfrias/plugins/git.lua
+++ vim/nvim/lua/dzfrias/plugins/git.lua
@@ -46,10 +46,11 @@ return {
     },
   },
 
-  -- Git interface
   {
-    'TimUntersberger/neogit',
-    cmd = 'Neogit',
-    keys = { { '<leader>g', '<Cmd>Neogit<CR>', desc = 'Open Neogit' } },
+    'kdheepak/lazygit.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    keys = { { '<leader>g', '<Cmd>LazyGit<CR>', desc = 'Open LazyGit' } },
   },
 }
