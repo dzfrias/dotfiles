fd12c5e702a83818f276a465aa09a5c4d895d4f4
Diego <96022404+dzfrias@users.noreply.github.com>
2023-05-28 20:26:44 -0700

    vim: clean up unused code

--- vim/nvim/lua/dzfrias/plugins/dap.lua
+++ vim/nvim/lua/dzfrias/plugins/dap.lua
@@ -120,48 +120,6 @@ return {
       },
     },
     config = function()
-      local dap = require 'dap'
-
-      local get_args = function()
-        local s = vim.fn.input 'Enter CLI args: '
-        return vim.fn.split(s, ' ')
-      end
-
-      dap.adapters.delve = {
-        type = 'server',
-        port = '${port}',
-        executable = {
-          command = 'dlv',
-          args = { 'dap', '-l', '127.0.0.1:${port}' },
-        },
-      }
-
-      local util = require 'dzfrias/util'
-      dap.configurations.go = {
-        {
-          type = 'delve',
-          name = 'Package',
-          request = 'launch',
-          program = '${file}',
-        },
-        {
-          type = 'delve',
-          name = 'Test',
-          request = 'launch',
-          mode = 'test',
-          program = './${relativeFileDirname}',
-        },
-        {
-          type = 'delve',
-          name = 'CLI',
-          request = 'launch',
-          program = function()
-            return util.get_main 'go'
-          end,
-          args = get_args,
-        },
-      }
-
       vim.api.nvim_set_hl(0, 'Breakpoint', { fg = '#ff9e64' })
       vim.fn.sign_define(
         'DapBreakpoint',
