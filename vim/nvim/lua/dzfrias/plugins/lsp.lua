211e522bf6c327179e559dfc31626a5fcb9d1944
Diego <96022404+dzfrias@users.noreply.github.com>
2023-05-31 21:51:11 -0700

    vim(py): use both pyright and mypy

--- vim/nvim/lua/dzfrias/plugins/lsp.lua
+++ vim/nvim/lua/dzfrias/plugins/lsp.lua
@@ -40,7 +40,8 @@ return {
 
       -- Setting up servers
       local lspconfig = require 'lspconfig'
-      local default_install = { 'gopls', 'cssls', 'svelte', 'tsserver' }
+      local default_install =
+        { 'pyright', 'gopls', 'cssls', 'svelte', 'tsserver' }
       for _, lsp in ipairs(default_install) do
         lspconfig[lsp].setup {
           on_attach = on_attach,
