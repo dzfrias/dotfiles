ddb875ceb276904333f8619d13488325c65fe6ff
Diego <96022404+dzfrias@users.noreply.github.com>
2023-07-18 12:56:30 -0700

    zsh(aliases): add cargo aliases

--- zsh/aliases/rust.zsh
+++ zsh/aliases/rust.zsh
@@ -6,6 +6,8 @@ alias ct='cargo nextest run'
 alias cdoc='cargo doc --open'
 alias rustc='~/.cargo/bin/rustc'
 alias ccl='cargo clippy --all'
+alias c='cargo'
+alias cin='cargo insta test --review'
 
 alias crm='bat Cargo.toml | sed "1,/^\[dependencies\]$/d" | sed "/^\[.*\]$/d" | sed "/^$/d" | fzf | cut -w -f 1 | xargs cargo remove'
 
