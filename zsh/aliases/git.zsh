f6f3477f285c0b185bf6e379d2ac353a17f58f71
Diego <96022404+dzfrias@users.noreply.github.com>
2023-10-07 21:11:23 -0700

    zsh: make some git changes

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -1,17 +1,3 @@
-# -GIT-
-# Alias for committing with a message and adding all
-alias gcm='gcam'
-# Makes bat the diff pager with no styling
 alias gd='git diff'
-# Amend git commit
-alias gamm='git commit --amend'
-# Counts all commits made on the current day
-alias gtod='git rev-list --count --after=today00:00:00 --all'
-alias gc='git add --all && git commit --verbose'
-alias gcomms='git rev-list --all --count'
-alias gcom='git checkout main'
+alias gst='git status'
 alias lg='lazygit'
-
-# -GITHUB-
-alias ghp='gh per'
-alias gho='gh repo view --web'
