5a4e0511ef90e5c74ba5e8345811cb70865f706c
Diego <96022404+dzfrias@users.noreply.github.com>
2023-10-09 18:27:31 -0700

    hs: add app resizer

---
+++ hammerspoon/app_resizer.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+local config = {
+  blacklist = {},
+}
+
+Watcher = hs.application.watcher.new(function(name, event, app)
+  if
+    event ~= hs.application.watcher.launched
+    or hs.fnutils.contains(config.blacklist, name)
+  then
+    return
+  end
+
+  local window = app:focusedWindow()
+  if not window then
+    return
+  end
+  window:maximize()
+end)
+
+--- Blacklist certain applications, by app name.
+--- @param apps string[]
+function M.blacklist(apps)
+  config.blacklist = apps
+end
+
+--- Start the app resizer.
+function M.start()
+  Watcher:start()
+end
+
+return M
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,8 @@
 require 'global'
 require 'apps'
 
+local resizer = require 'app_resizer'
+resizer.blacklist { 'Finder', 'System Settings' }
+resizer.start()
+
 hs.alert.show 'Loaded config'
