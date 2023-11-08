05ebf3cc6d2f9a6df4c52949cabc64a8c40bf6bf
Diego <96022404+dzfrias@users.noreply.github.com>
2023-11-07 17:47:41 -0800

    zsh(aliases): change rust aliases

--- zsh/aliases/rust.zsh
+++ zsh/aliases/rust.zsh
@@ -1,11 +1,8 @@
 # Cargo
 alias temprs='cargo init --name temprs'
 alias cr='cargo run'
-alias ct='cargo nextest run'
-alias cdoc='cargo doc --open'
+alias ct='cargo test --workspace'
 alias rustc='~/.cargo/bin/rustc'
-alias ccl='cargo clippy --all'
-alias cin='cargo insta test --review'
 
 alias crm='bat Cargo.toml | sed "1,/^\[dependencies\]$/d" | sed "/^\[.*\]$/d" | sed "/^$/d" | fzf | cut -w -f 1 | xargs cargo remove'
 
