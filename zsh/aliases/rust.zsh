2b64cc9cd69eeaae8fa4bcdd9ab1e14e14c61cf1
Diego <96022404+dzfrias@users.noreply.github.com>
2023-11-29 19:57:05 -0800

    brew: update brewfile
    Also add rust alias

--- zsh/aliases/rust.zsh
+++ zsh/aliases/rust.zsh
@@ -1,7 +1,7 @@
 # Cargo
 alias temprs='cargo init --name temprs'
 alias cr='cargo run'
-alias ct='cargo test --workspace'
+alias ct='cargo nextest run'
 alias rustc='~/.cargo/bin/rustc'
 
 alias crm='bat Cargo.toml | sed "1,/^\[dependencies\]$/d" | sed "/^\[.*\]$/d" | sed "/^$/d" | fzf | cut -w -f 1 | xargs cargo remove'
